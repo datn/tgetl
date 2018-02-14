4e59c6b840533bc6675da921b165bc7fde639553
d@n <git@eleven.org>
2018-02-13 16:32:54 -0800

    tweaks

--- json2sql.rb
+++ json2sql.rb
@@ -31,21 +31,27 @@ def process_hash(recordh, current_key, hash)
 		if value.is_a?(Hash)
 			process_hash(recordh, combined_key, value)
 		else
-			recordh[combined_key] = value
 			case value.class.to_s
     			when "String"
 				if /^[0-9]+\.[0-9]*$/.match(value) or /^\.[0-9]+$/.match(value)
 					$columns[combined_key]=2 if ( !$columns.key?(combined_key) ) or ( 2 > $columns[combined_key] )
-				elsif value == "spunt"
+				elsif value == ""
 					$columns[combined_key]=0
 				else
 					$columns[combined_key]=3 if ( !$columns.key?(combined_key) ) or ( 3 > $columns[combined_key] )
 				end
-			when "Integer","FalseClass","TrueClass"
+			when "Integer"
 				$columns[combined_key]=1 if ( !$columns.key?(combined_key) ) or ( 1 > $columns[combined_key] )
+			when "FalseClass"
+				$columns[combined_key]=1 if ( !$columns.key?(combined_key) ) or ( 1 > $columns[combined_key] )
+				value=0
+			when "TrueClass"
+				$columns[combined_key]=1 if ( !$columns.key?(combined_key) ) or ( 1 > $columns[combined_key] )
+				value=1
 			else
 				$columns[combined_key]=3 if ( !$columns.key?(combined_key) ) or ( 3 > $columns[combined_key] )
 			end
+			recordh[combined_key] = value
       			$lengths[combined_key] = value.to_s.length if ( ! $lengths[combined_key]) or ( value.to_s.length > $lengths[combined_key] )
 		end
 	end
@@ -55,8 +61,10 @@ File.readlines(jsonfile).each do |line|
 	recordh = {}
         eachone = JSON.parse(line)
 	process_hash(recordh, '', eachone)
+#	p recordh
 end
 
+p "----------------------------"
 p $columns
 p "----------------------------"
 p $lengths
